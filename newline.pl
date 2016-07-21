b1872a77b768975f32f4e0663aaf7e276d3d7cb9
JRonhovde <jronhovde90@gmail.com>
2016-07-21 17:45:44 +0000

    Simply regex, add stdout

--- newline.pl
+++ newline.pl
@@ -1,33 +1,43 @@
 #!/usr/bin/perl
-my @files = glob "/usr/local/apache/htdocs/jon/ss/*.php";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/*.inc";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/*.tab";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/admissions/*.php";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/admissions/*.inc";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/admissions/*.tab";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/Reports/0/*.php";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/Reports/0/*.inc";
-push @files, glob "/usr/local/apache/htdocs/jon/ss/Reports/0/*.tab";
-#for (0..$#files){
-      #$files[$_] =~ s/\.txt$//;
-  #}
-#my @files = glob "$dir
+
+my @files;
+if ( @ARGV > 1 ) {
+    @files = @ARGV;
+} elsif(@ARGV == 1) {
+    @files = glob "$ARGV[0]";
+} else {
+    print "Error: no files found\n";
+    exit;
+}
+
+if(@files < 1) {
+    print "Error: no files found\n";
+    exit;
+}
+
+print scalar @files." files to process.\n";
+
 foreach $file(@files){
 
+    # Set output back to STDOUT(terminal)
+    select STDOUT;
+    print "Processing $file\n";
 
-#Open the file and read data
-#Die with grace if it fails
+
+    #Open the file and read data
+    #Die with grace if it fails
     open (FILE, "<$file") or die "Can't open $file: $!\n";
     @lines = <FILE>;
     close FILE;
-#Open same file for writing, reusing STDOUT
-    open (STDOUT, ">$file") or die "Can't open $file: $!\n";
+    #Open same file for writing, using FF
+    open (FF, ">$file") or die "Can't open $file: $!\n";
 
+    # Select current file as output destination
+    select FF;
     my $header = 0;
     for ( @lines ) {
-        s/(^[\s\t]*print\([^(\\n)]*)\\n(\"\);$)/$1$2/;
+        s/(print\(.*?)\\n(?= *\" *\);)/$1/;
         print;
     }
-#Finish up
-    close STDOUT;
+    close FF;
 }
